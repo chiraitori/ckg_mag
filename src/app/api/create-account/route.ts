8feba5b5f2a9848a5db5e449fe2844b73d2f06b6
Chiraitori <77224364+tathaha@users.noreply.github.com>
2024-07-11 21:05:58 +0700

    fix routing build on vercel and cloudflare pages

--- src/app/api/create-account/route.ts
+++ src/app/api/create-account/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import { MongoClient } from 'mongodb';
 import { hash } from 'bcrypt';
+export const runtime = 'edge';
 
 export async function POST(request: Request) {
   try {
